aa118faa7c33a47f13d44bd44167654fe12d1840
ademkaragulle <ademkaragulle@outlook.com>
2023-06-15 14:00:48 +0300

    attempt-8.4

--- src/components/header/search/SearchItems.jsx
+++ src/components/header/search/SearchItems.jsx
@@ -22,13 +22,14 @@ function SearchItems({ value, setIsTrueDropDownMenu }) {
     })
 
     return (
-        <div>
+
+        <div className='search-items'>
             {loading && <Skeleton variant="rectangular" width="100%" height={300} sx={{ bgcolor: 'grey.900' }} />}
             {!loading && error ? <div>Error: {error}</div> : null}
             {!loading && data ? data.map((movie, index) => {
                 if (index < 4 && movie.poster_path) {
                     return (
-                        <Link onClick={() => setIsTrueDropDownMenu(false)} to={`movie-detail-${movie.id}`} key={index} >
+                        <Link onClick={() => setIsTrueDropDownMenu(false)} to={`movie-detail-${movie.id}`} key={index} className='search-item'>
                             <div className='search-img'><img className='img-fluid' src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="" /></div>
                             <div className='search-title'>{movie.original_title}</div>
                         </Link>

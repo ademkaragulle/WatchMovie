690bfadaf3f7d2a075f92ea92ac71bc930a3efa6
ademkaragulle <ademkaragulle@outlook.com>
2023-06-15 13:46:28 +0300

    Revert "attempt-8"
    This reverts commit 74ac84277cd3b3d556890f2c4b01e31f5f8191be.

--- src/components/header/search/SearchItems.jsx
+++ src/components/header/search/SearchItems.jsx
@@ -5,7 +5,6 @@ import { Skeleton } from '@mui/material'
 import { Link } from 'react-router-dom'
 
 function SearchItems({ value, setIsTrueDropDownMenu }) {
-    console.log('adem')
     const dispatch = useDispatch()
 
     useEffect(() => {

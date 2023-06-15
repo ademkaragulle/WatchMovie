f00b109e1e7e7930cd29e3b63f220a5db97bb94d
ademkaragulle <ademkaragulle@outlook.com>
2023-06-15 13:48:43 +0300

    sadas

--- src/components/header/search/SearchItems.jsx
+++ src/components/header/search/SearchItems.jsx
@@ -5,6 +5,8 @@ import { Skeleton } from '@mui/material'
 import { Link } from 'react-router-dom'
 
 function SearchItems({ value, setIsTrueDropDownMenu }) {
+
+    console.log('adem')
     const dispatch = useDispatch()
 
     useEffect(() => {
